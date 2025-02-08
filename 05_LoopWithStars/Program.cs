7a38f54cc21037961e5d5f2ea646b6ef616d4e83
ömer ensari <omerensari51@gmail.com>
2025-02-08 18:04:30 +0300

    Ders 5 Güncellenmis

--- 05_LoopWithStars/Program.cs
+++ 05_LoopWithStars/Program.cs
@@ -87,6 +87,40 @@ namespace _05_LoopWithStars
 
             #endregion
 
+            #region Baklava dilimi
+
+            int n = 5;
+
+            // Üst Kýsým
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Alt Kýsým
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+
             #region Ters pramit
 
             //int n = 5;
